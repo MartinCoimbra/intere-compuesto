b480592ac8a3902befb37af397409d4acbb7f093
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-11-16 15:42:58 -0300

    format prize

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,34 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Section from "./components/Section";
+import Balance from "./components/Balance";
+import { useState } from "react";
+
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit;
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1);
+  }
+  return Math.round(total);
+};
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
 
 const App = () => {
-  const handleSubmit = () => {
-    /*  */
+  const [balance, setBalance] = useState("");
+  const handleSubmit = ({ deposit, contribution, years, rate }) => {
+    const val = compoundInterest(
+      Number(deposit),
+      Number(contribution),
+      Number(years),
+      Number(rate)
+    );
+    setBalance(formatter.format(val));
   };
 
   return (
@@ -24,6 +48,7 @@ const App = () => {
             <Button>Calcular</Button>
           </Form>
         </Formik>
+        {balance !== "" && <Balance>Balance final: {balance}</Balance>}
       </Section>
     </Container>
   );
